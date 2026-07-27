b1ed267577355a7c645a4963513550d7c8e0ca48
unknown <joylinmathias30@gmail.com>
2026-07-27 21:57:22 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-export default function Home() { //react component called Home
+import Hero from "@/components/Hero";
+
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
-      <h1 className="text-6xl font-bold">
-        Joylin Mathias
-      </h1>
-
-      <p className="mt-6 text-xl text-gray-300">
-        Building technology with purpose.
-      </p>
-
-      <p className="mt-3 text-gray-500">
-        Portfolio under construction 🚀
-      </p>
+    <main>
+      <Hero />
     </main>
   );
 }
