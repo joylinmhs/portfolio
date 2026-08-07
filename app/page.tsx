0cfd4adb25f08691a72b6c623ccab5b54edd6563
unknown <joylinmathias30@gmail.com>
2026-08-07 23:07:24 +0530

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
 
-
 export default function Home() {
   return (
     <main>
