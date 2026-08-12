313a6b9c5d52edbf4acbb196d7b733efce6ccee7
unknown <joylinmathias30@gmail.com>
2026-08-12 22:16:15 +0530

    Updated ProjectCard.tsx

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["http://localhost:3000", "http://192.168.1.8:3000"],
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://192.168.1.8:3000",
+    "http://172.17.0.137:3000",
+    "http://172.17.0.137:3001",
+  ],
 };
 
 export default nextConfig;
